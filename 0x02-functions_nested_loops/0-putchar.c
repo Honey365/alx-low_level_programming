57ff98ce2059f745e15af65eac1cdb30c5868ec4
Honey365 <honey36510@gmail.com>
2023-09-12 17:17:41 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,21 @@
-# include "main.h"
-
+#include "main.h"
 /**
- *main - print out main car by char.
- *Description: uses the main header file
- *that contains the function _putchar
- *Return: 0.
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
 
 int main(void)
 {
-char str[] = "_putchar";
-int i;
-
-i = 0;
-while (str[i])
-{
-_putchar(str[i]);
-++i;
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
-
 return (0);
+}
 
