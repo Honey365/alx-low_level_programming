23443b60ef9666cca82ae60eea37637dec51dfa5
Honey365 <honey36510@gmail.com>
2023-10-04 14:38:48 -0700

    malloc free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,18 +7,18 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *p;
-	unsigned int i;
-	
-	if (size > 0)
-	{
-		p = malloc(size * sizeof(char));
-		if (p == NULL)
-			return (NULL);
-	}
-	else
-		return (NULL);
-	for (i = 0; i < size; i++)
-		p[i] = c;
-	return (p);
+char *p;
+unsigned int i;
+
+if (size > 0)
+{
+p = malloc(size * sizeof(char));
+if (p == NULL)
+return (NULL);
+}
+else
+return (NULL);
+for (i = 0; i < size; i++)
+p[i] = c;
+return (p);
 }
